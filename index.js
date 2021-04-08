eb17a42f4c528d676f91f2f252530185c710939e
unknown <jonathancalderonsilberman@gmail.com>
2021-04-08 09:57:49 -0700

    app builded

---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9000
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/", (_, res) => {
+    res.json({ data: "API is accounted for" })
+})
+
+app.listen(port, () => {
+    console.log(`Server is alive on ${port}`)
+});
+
+console.log("its alive!!!!")
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USERNAME)
+console.log(process.env.PORT)
