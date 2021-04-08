caad5beb5cd43d7594d2a004ef66a1bb2da35d59
unknown <jonathancalderonsilberman@gmail.com>
2021-04-08 10:51:58 -0700

    dynamic data set

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,19 @@ function url(path) {
 }
 
 function App() {
+  const [data, setData] = useState("Hi")
+
+  useEffect(() => {
+    fetch(url("/api/"))
+      .then(res => res.json())
+      .then(apiData => setData(apiData.data))
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        {data}
       </header>
     </div>
   );
