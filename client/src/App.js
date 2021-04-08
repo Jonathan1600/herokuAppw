7a14585f2e9a0905fe4e94c24da59c333f9e4e25
unknown <jonathancalderonsilberman@gmail.com>
2021-04-08 10:41:19 -0700

    react app builded

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function url(path) {
+  return process.env.NODE_ENV === "development" ? `http://localhost:1234${path}` : path
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
